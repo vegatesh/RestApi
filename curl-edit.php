bb27c6fb139f2c36316b225e2d3ee67c2d789423
vengatesh <42336841+vegatesh@users.noreply.github.com>
2018-10-03 15:40:55 +0530

    Update curl-edit.php

--- curl-edit.php
+++ curl-edit.php
@@ -1,21 +1,30 @@
 <?php
-$authtoken= "*****";
+	$authtoken="f4896d5f7d925f52ab9977e87463ba92";
 
-$url = "https://creator.zoho.com/api/aorborctechnologiescpt/json/quotes-vengatesh/form/Sheet1_Report/record/update";
+	curl_edit();
 
-$post_fields= array();
-$post_fields['authtoken']= "*****";
-$post_fields['scope']= "creatorapi";
-$post_fields['criteria']= "ID == 3682220000000024259";
-$post_fields['Added_Location'] = "Chennai";
+	function curl_edit()
+	{
+	
+		$url = "https://creator.zoho.com/api/aorborctechnologiescpt/json/trailvengatesh/form/Contact/record/update";
 
+		
+		$post_fields = array();
+		$post_fields['authtoken'] = "f4896d5f7d925f52ab9977e87463ba92";
+		$post_fields['scope'] = "creatorapi";
+		$post_fields['criteria'] = "Emp_No == 2";
+		$post_fields['Emp_No'] ="5";
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch,CURLOPT_POST,true);
-curl_setopt($ch,CURLOPT_POSTFIELDS,$post_fields);
-$response = curl_exec($ch);
-curl_close($ch);
-print_r($response);
-?>
+		$ch = curl_init($url);
+
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch,CURLOPT_POST,true);
+		curl_setopt($ch,CURLOPT_POSTFIELDS,$post_fields);
+
+		$response = curl_exec($ch);
+		curl_close($ch);
+		echo $response;
+
+	}
+
+	?>
