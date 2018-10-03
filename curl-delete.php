81904588d93cb00055c4c5307c7658bb1eb8e862
vengatesh <42336841+vegatesh@users.noreply.github.com>
2018-10-03 15:39:50 +0530

    Update curl-delete.php

--- curl-delete.php
+++ curl-delete.php
@@ -1,20 +1,30 @@
 <?php
-$authtoken= "*****";
 
-$url= "https://creator.zoho.com/api/aorborctechnologiescpt/json/quotes-vengatesh/form/trialform/record/delete";
+$authtoken="f4896d5f7d925f52ab9977e87463ba92";
 
-$post_fields= array();
-$post_fields['authtoken']= "******";
-$post_fields['scope']= "creatorapi";
-$post_fields['criteria']= ID == "3696069000000063067";
+curl_delete();
 
+function curl_delete()
+{
+
+$url = "https://creator.zoho.com/api/aorborctechnologiescpt/json/trailvengatesh/form/Contact/record/delete";
+
+$post_fields = array();
+$post_fields['authtoken'] = "f4896d5f7d925f52ab9977e87463ba92";
+$post_fields['scope'] = "creatorapi";
+$post_fields['criteria'] = "(Emp_No == 1)";
 
 $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch,CURLOPT_POST,true);
 curl_setopt($ch,CURLOPT_POSTFIELDS,$post_fields);
+
 $response = curl_exec($ch);
 curl_close($ch);
-print_r($response);
+echo $response;
+
+}
+
 ?>
